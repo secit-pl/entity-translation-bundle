4bd909e1b2cfcf2a8d543bc7a80b45492339861d
secit-pl <t.gemza@secit.pl>
2018-12-05 09:15:51 +0100

    Fixed Symfony 4.2 deprecations

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,8 +19,15 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('entity_translation');
+        $treeBuilder = new TreeBuilder('entity_translation');
+
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('entity_translation');
+        }
+
         $rootNode
             ->children()
                 ->arrayNode('locales')
